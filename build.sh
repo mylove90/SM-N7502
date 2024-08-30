c7aaddf159ad0cc932daf175458ab060981aa9b1
Mohamed Saad <50009562+mylove90@users.noreply.github.com>
2024-08-30 03:54:12 +0000

    hl3gds_defconfig: Build all modules inline

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#rm -rf output
+rm -rf output
 export ARCH=arm
 if ! d [ arm-linux-androideabi-4.9 ]; then
 git clone -b android-8.0.0_r51 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/
@@ -8,5 +8,5 @@ export CROSS_COMPILE=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 
 mkdir output
 
-make -C $(pwd) O=output msm8226-sec_defconfig VARIANT_DEFCONFIG=msm8226-sec_hl3gds_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output hl3gds_defconfig
 make -j$(nproc) -C $(pwd) O=output
